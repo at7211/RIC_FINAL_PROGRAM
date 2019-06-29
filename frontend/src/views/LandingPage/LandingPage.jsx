95ef13b2c1cceeec0e59de1ad7d2e06fa0b5c13b
at7211 <at7211@gmail.com>
2019-06-29 22:35:35 +0800

    add logo animation

--- frontend/src/views/LandingPage/LandingPage.jsx
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -9,7 +9,6 @@ import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.jsx";
 import Footer from "./Sections/Footer";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
@@ -23,28 +22,134 @@ import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.js
 // import ProductSection from "./Sections/ProductSection.jsx";
 // import TeamSection from "./Sections/TeamSection.jsx";
 // import WorkSection from "./Sections/WorkSection.jsx";
+import LOGO from '../../assets/img/NTUCEP_LOGO.svg';
 import SectionCarousel from "./Sections/SectionCarousel.jsx";
 import Collection from "./Sections/Collection.jsx";
 
-const dashboardRoutes = [];
+const styles= {
+  wrapper: {
+    background: '#fff',
+    height: '100%',
+  },
+  buttonWrapper: {
+    display: 'flex',
+    width: '100vw',
+    height: '100vh',
+    justifyContent : 'center',
+    alignItems: 'center',
+  },
+  logoWrapper: {
+    display: 'flex',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    border: 0,
+    position: 'relative',
+    opacity: 0,
+    right: -150,
+  },
+  titleWrapper: {
+    position: 'relative',
+    top: 6,
+    left: -140,
+    opacity: 0,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 28,
+    margin: '6px 0',
+    fontWeight: 550,
+  },
+  show: {
+    transitionProperty: 'opacity',
+    transitionDuration: '1s',
+    opacity: 1,
+  },
+  showTitleWrapper: {
+    transitionProperty: 'opacity, left',
+    transitionDuration: '0.8s',
+    opacity: 1,
+    left: 10
+  },
+  showLogo: {
+    transitionProperty: 'opacity, right',
+    transitionDuration: '0.8s',
+    opacity: 1,
+    right: 10
+  },
+  hide: {
+    transitionProperty: 'opacity',
+    transitionDuration: '1s',
+    opacity: 0,
+  },
+}
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+      open: false,
+      opening: false,
+    })
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ opening: true })
+    }, 2000);
+    setTimeout(() => {
+      this.setState({ open: true })
+    }, 10000)
+  }
+
   render() {
-    const { classes, ...rest } = this.props;
+    const { classes } = this.props;
+    const { open, opening } = this.state;
 
     return (
-      <div>
-
-        <SectionCarousel />
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div className={classes.container}>
-            {/* <ProductSection />
-            <TeamSection />
-            <WorkSection /> */}
-            <Collection />
+      <div style={styles.wrapper}>
+        <div style={
+          open ? {
+          ...styles.buttonWrapper,
+          display: 'none',
+          } : {
+          ...styles.buttonWrapper,
+          ...styles.show
+          }
+        }>
+          <button
+            type="button"
+            style={
+              opening ? {
+                ...styles.logo,
+                ...styles.showLogo
+              } : styles.logo}
+            onClick={() => this.setState({open: true})}>
+            <img src={LOGO}  alt="NTUCEP-logo"/>
+          </button>
+          <div style={
+            opening ? {
+              ...styles.titleWrapper,
+              ...styles.showTitleWrapper
+            } : styles.titleWrapper}>
+            <span style={styles.title}>臺大</span>
+            <span style={styles.title}>創創學程</span>
           </div>
         </div>
-        <Footer />
+        <div style={open ? styles.show : {...styles.hide, height: 0} }>
+          <SectionCarousel />
+            <div className={classNames(classes.main, classes.mainRaised)}>
+              <div className={classes.container}>
+                {/* <ProductSection />
+                <TeamSection />
+                <WorkSection /> */}
+                <Collection />
+              </div>
+            </div>
+          <Footer />
+        </div>
       </div>
     );
   }
